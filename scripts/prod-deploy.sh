cf037052c35768cf247744d82165d107bd50bee6
Obi.M <mail@obimadu.pro>
2024-07-18 00:11:56 +0100

    feat: use systemctl service

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -1,14 +1,14 @@
-# get to project root
-cd $(git rev-parse --show-toplevel)/src/Hng.Web
+# navigate to repo root
+cd  $(git rev-parse --show-toplevel)
+
 # install dependencies
-dotnet restore ./Hng.Web.csproj
+dotnet restore Hng.Csharp.Web.sln
+
 # build app
 dotnet build
+
 # publish app
 dotnet publish
-# ensure app is executable
-chmod +x ./bin/Release/net8.0/Hng.Web
-# kill any existing instance
-sudo kill -9 $(sudo lsof -t -i:5000) &> /dev/null || true
-# run app - prod
-./bin/Release/net8.0/Hng.Web
+
+# kill any existing instance on port 80
+sudo systemctl restart hng-web
