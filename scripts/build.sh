c99ae753bfc1f12c26d70c7588e503ff0a67aba9
Obi.M <mail@obimadu.pro>
2024-07-17 13:05:34 +0100

    build: update build script; scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,3 @@ cd $(git rev-parse --show-toplevel)/src/Hng.Web
 dotnet restore ./Hng.Web.csproj
 # build app
 dotnet build
-# publish app
-dotnet publish
-# ensure app is executable
-chmod +x ./bin/Release/net8.0/Hng.Web
-# kill any existing instance
-sudo kill -9 $(sudo lsof -t -i:5000) &> /dev/null || true
-# run app
-./bin/Release/net8.0/Hng.Web
