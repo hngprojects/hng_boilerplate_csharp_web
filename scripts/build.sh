c14f2c87ac8b1f0e4ab180b6a4e509f01541a955
Obi.M <mail@obimadu.pro>
2024-07-17 12:05:44 +0100

    feat: upgrade build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
-cd ../src
-dotnet restore Hng.Web/Hng.Web.csproj
-cd Hng.Web
+# get to project root
+cd $(git rev-parse --show-toplevel)/src/Hng.Web
+# install dependencies
+dotnet restore ./Hng.Web.csproj
+# build app
 dotnet build
+# publish app
 dotnet publish
+# ensure app is executable
 chmod +x ./bin/Release/net8.0/Hng.Web
+# kill any existing instance
+sudo kill -9 $(sudo lsof -t -i:5000) &> /dev/null || true
+# run app
 ./bin/Release/net8.0/Hng.Web
