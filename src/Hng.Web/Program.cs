750f3e27d4f255f59113bb16ef3bf8af3a1d5d03
Ubong Edem <ubongedem78@gmail.com>
2024-07-19 20:38:32 +0100

    feat:#30-add-logging

--- src/Hng.Web/Program.cs
+++ src/Hng.Web/Program.cs
@@ -1,12 +1,14 @@
+using System.Net;
 using Hng.Web.Services;
+using Microsoft.AspNetCore.Diagnostics;
 using NLog;
 using NLog.Extensions.Logging;
+using NLog.Fluent;
 using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure NLog
-builder.Host.UseNLog(); // Use NLog for Dependency Injection
+builder.Host.UseNLog();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -25,6 +27,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Response.ContentType = "text/plain";
+
+        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (contextFeature != null)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Error(contextFeature.Error, "Unhandled exception");
+
+            await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+        }
+    });
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
