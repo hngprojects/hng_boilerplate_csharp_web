8899bcb23b9accb97bfc50555ce61bbd2402cade
Uchechukwu Emmanuel <daleentontech@gmail.com>
2024-07-22 00:52:36 +0100

    refactor: add authorization

--- src/Hng.Web/Controllers/JobListingController.cs
+++ src/Hng.Web/Controllers/JobListingController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Hng.Application.Dto;
 using Hng.Application.Interfaces;
@@ -7,6 +8,7 @@ namespace Hng.Web.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class JobsController : ControllerBase
     {
         private readonly IJobListingService _jobListingService;
