77e0117224b32f2007b76717134c789d34d9be19
Juninho <juninhopeter1@gmail.com>
2024-07-21 09:02:46 +0100

    feat(product): add endpoint for product search

---
+++ src/Hng.Application/Models/SearchParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hng.Application.Models
+{
+    public class SearchParameters
+    {
+        [Required(ErrorMessage = "Name is a required parameter")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
---
+++ src/Hng.Web/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Hng.Application.Interfaces;
+using Hng.Application.Models;
+using Hng.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers
+{
+    [Route("api/v1/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("search")]
+
+        public IActionResult SearchProducts([FromQuery] SearchParameters parameters)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(new
+                {
+                    success = false,
+                    errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => new { parameter = e.ErrorMessage.Split(' ')[0], message = e.ErrorMessage })),
+                    statusCode = 422
+                });
+            }
+
+            try
+            {
+                var products = _productService.SearchProducts(parameters);
+
+                if (!products.Any())
+                {
+                    return Ok(new
+                    {
+                        success = true,
+                        products = new List<Product>(),
+                        statusCode = 204
+                    });
+                }
+
+                return Ok(new
+                {
+                    success = true,
+                    statusCode = 200,
+                    products,
+                });
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return StatusCode(500, new
+                {
+                    success = false,
+                    message = "Server Error",
+                    statusCode = 500
+                });
+            }
+        }
+    }
+}
+
