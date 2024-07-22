1212ba89986cff8f78790efb3e9216980a508a0a
chimaihueze <drkingsleyc@gmail.com>
2024-07-22 23:21:19 +0100

    added relevant fields to Organization and OrganizationDto

--- src/Hng.Application/Features/Organisations/Dtos/OrganizationDto.cs
+++ src/Hng.Application/Features/Organisations/Dtos/OrganizationDto.cs
@@ -1,9 +1,46 @@
-namespace Hng.Application.Features.Organisations.Dtos
+using System.Text.Json.Serialization;
+using Hng.Application.Features.UserManagement.Dtos;
+
+public class OrganizationDto
 {
-    public class OrganizationDto
-    {
-        public string org_id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-    }
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("id")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("slug")]
+    public string Slug { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("industry")]
+    public string Industry { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
+
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    [JsonPropertyName("state")]
+    public string State { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("owner_id")]
+    public string OwnerId { get; set; }
+
+    [JsonPropertyName("users")]
+    public ICollection<UserDto> Users { get; set; }
 }
