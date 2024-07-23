bde560a59429666c292ff78a38f49f1e54216bb7
chimaihueze <drkingsleyc@gmail.com>
2024-07-23 19:29:19 +0100

    feat: added controller to handle requests for organization

---
+++ src/Hng.Web/Controllers/OrganizationController.cs
@@ -0,0 +1,29 @@
+﻿using Hng.Application.Features.Organisations.Commands;
+using Hng.Application.Features.Organisations.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers;
+
+[ApiController]
+[Route("api/v1/organizations")]
+public class OrganizationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public OrganizationController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    //[Authorize]
+    [HttpPost]
+    [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status201Created)]
+    public async Task<ActionResult<OrganizationDto>> CreateOrganization([FromBody] CreateOrganizationDto body)
+    {
+        var command = new CreateOrganizationCommand(body);
+        var response = await _mediator.Send(command);
+        return CreatedAtAction(nameof(CreateOrganization), response);
+    }
+}
