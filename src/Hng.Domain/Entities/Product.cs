03722655674e479648c712215072b56881dbd2c9
dmendie <116080582+dmendie@users.noreply.github.com>
2024-07-23 20:45:17 +0100

    Update Product.cs

--- src/Hng.Domain/Entities/Product.cs
+++ src/Hng.Domain/Entities/Product.cs
@@ -14,7 +14,6 @@ public class Product : EntityBase
     public decimal Price { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; }
-
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
