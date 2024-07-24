7a96c567f9ad0bfd40cb94aa2d1edc4ee4a42b94
chimaihueze <drkingsleyc@gmail.com>
2024-07-24 15:42:15 +0100

    feat: added GET org by id endpoint

--- src/Hng.Web/Controllers/OrganizationController.cs
+++ src/Hng.Web/Controllers/OrganizationController.cs
@@ -1,16 +1,32 @@
 ﻿using Hng.Application.Features.Organisations.Commands;
 using Hng.Application.Features.Organisations.Dtos;
+using Hng.Application.Features.Organisations.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hng.Web.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/v1/organizations")]
 public class OrganizationController(IMediator mediator) : ControllerBase
 {
-    [Authorize]
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status200OK)]
+    public async Task<ActionResult<OrganizationDto>> GetOrganizationById(Guid id)
+    {
+        var query = new GetOrganizationByIdQuery(id);
+        var response = await mediator.Send(query);
+        return response is null ? NotFound(new
+        {
+            message = "Organization not found",
+            is_successful = false,
+            status_code = 404
+        }) : Ok(response);
+    }
+
     [HttpPost]
     [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status201Created)]
     public async Task<ActionResult<OrganizationDto>> CreateOrganization([FromBody] CreateOrganizationDto body)
