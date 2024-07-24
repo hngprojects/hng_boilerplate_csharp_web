cc41604d02cf96030ff02af666818b8d71bf3a93
Tahir <olawaletech2@gmail.com>
2024-07-24 22:59:51 +0100

    Invite feature setup

---
+++ src/Hng.Domain/Entities/OrganisationInvite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hng.Domain.Entities
+{
+	public class OrganisationInvite
+	{
+		public Guid Id { get; set; }
+		public Guid Organization_id { get; set; }
+		public string Name { get; set; }
+		public string Invite_email { get; set; }
+		public string Status { get; set; }
+		public DateTime Created_at { get; set;}
+		public DateTime Expires_at { get; set; }
+		public DateTime Accepted_at { get; set; }
+		public string Invite_link { get; set; }
+	}
+}
---
+++ src/Hng.Web/Controllers/InviteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class InviteController : ControllerBase
+	{
+		public InviteController() 
+		{ 
+		 
+		
+		}
+
+		[HttpPost]
+		public async Task<> Invite(InviteModel request)
+		{
+		}
+
+		[HttpGet("accept")]
+		public async Task<> Accept(string token)
+		{
+		}
+	}
+}
