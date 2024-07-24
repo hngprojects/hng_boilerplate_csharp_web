f681e4798341ce60b3310440b2064ce6626d0c36
chimaihueze <drkingsleyc@gmail.com>
2024-07-24 15:11:29 +0100

    refactor: modifications to controller

--- src/Hng.Web/Controllers/OrganizationController.cs
+++ src/Hng.Web/Controllers/OrganizationController.cs
@@ -8,22 +8,15 @@ namespace Hng.Web.Controllers;
 
 [ApiController]
 [Route("api/v1/organizations")]
-public class OrganizationController : ControllerBase
+public class OrganizationController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public OrganizationController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [Authorize]
     [HttpPost]
     [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status201Created)]
     public async Task<ActionResult<OrganizationDto>> CreateOrganization([FromBody] CreateOrganizationDto body)
     {
         var command = new CreateOrganizationCommand(body);
-        var response = await _mediator.Send(command);
+        var response = await mediator.Send(command);
         return CreatedAtAction(nameof(CreateOrganization), response);
     }
 }
