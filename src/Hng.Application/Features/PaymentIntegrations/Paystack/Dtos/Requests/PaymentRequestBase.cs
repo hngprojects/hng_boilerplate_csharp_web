28de659a11f07571218e7add7fdb4ad6185fec56
kenelight4u <kenelight4u@gmail.com>
2024-07-25 11:23:17 +0100

    chore: remove summary

--- src/Hng.Application/Features/PaymentIntegrations/Paystack/Dtos/Requests/PaymentRequestBase.cs
+++ src/Hng.Application/Features/PaymentIntegrations/Paystack/Dtos/Requests/PaymentRequestBase.cs
@@ -4,9 +4,6 @@ namespace Hng.Application.Features.PaymentIntegrations.Paystack.Dtos.Requests
 {
     public abstract record PaymentRequestBase
     {
-        /// <summary>
-        /// Required - The current business' SECRET KEY
-        /// </summary>
 		[JsonIgnore]
         public string BusinessAuthorizationToken { get; set; }
     }
