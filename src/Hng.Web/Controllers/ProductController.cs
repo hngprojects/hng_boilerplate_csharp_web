cac2e760540c99faeb5ca1be38ec68ecf1e3698c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-07-26 11:43:36 +0300

    fix: version conflict

--- src/Hng.Web/Controllers/ProductController.cs
+++ src/Hng.Web/Controllers/ProductController.cs
@@ -3,9 +3,6 @@ using Hng.Application.Features.Products.Queries;
 using Hng.Application.Features.Products.Dtos;
 using MediatR;
 using Hng.Application.Features.Products.Commands;
-using Hng.Application.Features.Products.Dtos;
-using Hng.Application.Features.Products.Queries;
-using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -43,38 +40,38 @@ namespace Hng.Web.Controllers
                 status_code = 200,
                 product
             });
-
-            /// <summary>
-            /// Product Deletion - deletes a product owned by a specific user
-            /// </summary>
-            [HttpDelete("{id}")]
-            [Authorize]
-            [ProducesResponseType(StatusCodes.Status204NoContent)]
-            public async Task<ActionResult> DeleteProductById(Guid id)
+        }
+        /// <summary>
+        /// Product Deletion - deletes a product owned by a specific user
+        /// </summary>
+        [HttpDelete("{id}")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> DeleteProductById(Guid id)
+        {
+            var command = new DeleteProductByIdCommand(id);
+            var deletedProduct = await _mediator.Send(command);
+            return deletedProduct is not null ? NoContent() : NotFound(new
             {
-                var command = new DeleteProductByIdCommand(id);
-                var deletedProduct = await _mediator.Send(command);
-                return deletedProduct is not null ? NoContent() : NotFound(new
-                {
-                    status_code = 404,
-                    message = "Product not found"
-                });
-            }
+                status_code = 404,
+                message = "Product not found"
+            });
+        }
 
-            /// <summary>
-            /// Product Categories - gets all categories for products 
-            /// </summary>
-            [HttpGet("categories")]
-            [Authorize]
-            [ProducesResponseType(typeof(CategoryDto), StatusCodes.Status200OK)]
-            public async Task<ActionResult<IEnumerable<CategoryDto>>> GeProductCategories()
+        /// <summary>
+        /// Product Categories - gets all categories for products
+        /// </summary>
+        [HttpGet("categories")]
+        [Authorize]
+        [ProducesResponseType(typeof(CategoryDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GeProductCategories()
+        {
+            var categories = await _mediator.Send(new GetCategoriesQuery());
+            return Ok(new
             {
-                var categories = await _mediator.Send(new GetCategoriesQuery());
-                return Ok(new
-                {
-                    status_code = 200,
-                    categories
-                });
-            }
+                status_code = 200,
+                categories
+            });
         }
     }
+}
