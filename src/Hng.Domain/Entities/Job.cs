2232c89d4d5f50334a47e12088ef66c6d1dc3a7a
chimaihueze <drkingsleyc@gmail.com>
2024-07-27 19:17:14 +0100

    feat: added Job class

---
+++ src/Hng.Domain/Entities/Job.cs
@@ -0,0 +1,14 @@
+﻿using Hng.Domain.Enums;
+
+namespace Hng.Domain.Entities;
+
+public class Job : EntityBase
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public double Salary { get; set; }
+    public JobType Type { get; set; }
+    public string Company { get; set; }
+    public DateTime DatePosted { get; set; }
+}
