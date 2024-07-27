d7badc3a5b6e942fb3dd42a9eba49efc51d3d8b5
chimaihueze <drkingsleyc@gmail.com>
2024-07-27 19:15:16 +0100

    feat: added a CreateJobCommandHandler to handle the CreateJobCommand request

---
+++ src/Hng.Application/Features/Jobs/Handlers/CreateJobCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Hng.Application.Features.Jobs.Commands;
+using Hng.Application.Features.Jobs.Dtos;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+
+namespace Hng.Application.Features.Jobs.Handlers;
+
+public class CreateJobCommandHandler(IRepository<Job> jobRepository, IMapper mapper)
+    : IRequestHandler<CreateJobCommand, JobDto>
+{
+    public async Task<JobDto> Handle(CreateJobCommand request, CancellationToken cancellationToken)
+    {
+        var job = mapper.Map<Job>(request.JobBody);
+        job.DatePosted = DateTime.UtcNow;
+
+        await jobRepository.AddAsync(job);
+        await jobRepository.SaveChanges();
+        return mapper.Map<JobDto>(job);
+    }
+}
