5895fd48c23c319b93654222483189ec870e9968
chimaihueze <drkingsleyc@gmail.com>
2024-07-27 19:22:05 +0100

    feat: added CreateJobDto class

---
+++ src/Hng.Application/Features/Jobs/Dtos/CreateJobDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using Hng.Domain.Enums;
+
+namespace Hng.Application.Features.Jobs.Dtos;
+
+public class CreateJobDto
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+    
+    [JsonPropertyName("location")]
+    public string Location { get; set; }
+    
+    [JsonPropertyName("salary")]
+    public double Salary { get; set; }
+    
+    [JsonPropertyName("type")]
+    public JobType Type { get; set; }
+    
+    [JsonPropertyName("company")]
+    public string Company { get; set; }
+}
