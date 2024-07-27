e8609b5f7276ad0f70d1aa43cb441673c62265c2
chimaihueze <drkingsleyc@gmail.com>
2024-07-27 19:39:39 +0100

    feat: added JobController with CreateJob endpoint

---
+++ src/Hng.Web/Controllers/JobController.cs
@@ -0,0 +1,21 @@
+﻿using Hng.Application.Features.Jobs.Commands;
+using Hng.Application.Features.Jobs.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers;
+
+[ApiController]
+[Route("api/v1/jobs")]
+public class JobController(IMediator mediator) : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(JobDto), StatusCodes.Status201Created)]
+    public async Task<ActionResult<JobDto>> CreateJob([FromBody] CreateJobDto body)
+    {
+        var command = new CreateJobCommand(body);
+        var response = await mediator.Send(command);
+        return CreatedAtAction(nameof(CreateJob), response);
+    }
+}
