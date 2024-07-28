49694aec64f0e6dadd8724d532e4f23e155629b2
kenelight4u <kenelight4u@gmail.com>
2024-07-28 14:35:05 +0100

    chore: added subscription schema

---
+++ src/Hng.Domain/Entities/Subscription.cs
@@ -0,0 +1,39 @@
+﻿using Hng.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hng.Domain.Entities
+{
+    public class Subscription : EntityBase
+    {
+        public Guid? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public Guid? OrganizationId { get; set; }
+
+        [ForeignKey("OrganizationId")]
+        public Organization Organization { get; set; }
+
+        public Guid? TransactionId { get; set; }
+
+        [ForeignKey("TransactionId")]
+        public Transaction Transaction { get; set; }
+
+        public SubscriptionPlan Plan { get; set; }
+
+        public SubscriptionFrequency Frequency { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
