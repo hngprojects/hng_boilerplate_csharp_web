8de771475a6f000705bac7b4845d277d0120b533
chimaihueze <drkingsleyc@gmail.com>
2024-07-29 20:28:52 +0100

    feat: added DeleteJobCommandHandler

---
+++ src/Hng.Application/Features/Jobs/Handlers/DeleteJobCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Hng.Application.Features.Jobs.Commands;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+
+namespace Hng.Application.Features.Jobs.Handlers;
+
+public class DeleteJobCommandHandler : IRequestHandler<DeleteJobCommand, bool>
+{
+    private readonly IRepository<Job> _jobRepository;
+
+    public DeleteJobCommandHandler(IRepository<Job> jobRepository)
+    {
+        _jobRepository = jobRepository;
+    }
+
+    public async Task<bool> Handle(DeleteJobCommand request, CancellationToken cancellationToken)
+    {
+        var job = await _jobRepository.GetBySpec(j => j.Id == request.JobId);
+        if (job == null)
+        {
+            return false;
+        }
+        await _jobRepository.DeleteAsync(job);
+        await _jobRepository.SaveChanges();
+        return true;
+    }
+}
