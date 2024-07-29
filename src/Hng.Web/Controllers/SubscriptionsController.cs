9c78d13e2c4fb130d8c23dc9f5bae1e186829b50
kenelight4u <kenelight4u@gmail.com>
2024-07-29 07:52:13 +0100

    feat: implement subscribe free plan

---
+++ src/Hng.Web/Controllers/SubscriptionsController.cs
@@ -0,0 +1,33 @@
+﻿using Hng.Application.Features.Subscriptions.Dtos.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers
+{
+    [ApiController]
+    [Route("api/v1/subscriptions")]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SubscriptionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// To subscribe for Free Plan
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> SubscribeFreePlan([FromBody] SubscribeFreePlan command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (result.IsSuccess)
+                return Ok(result.Value);
+
+            return BadRequest(result.Error);
+        }
+    }
+}
