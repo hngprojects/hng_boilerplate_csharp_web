4e14acc613d32b2ceecccc9e117eee2c158ae413
chimaihueze <drkingsleyc@gmail.com>
2024-07-29 20:28:30 +0100

    feat: added DeleteJobCommand

---
+++ src/Hng.Application/Features/Jobs/Commands/DeleteJobCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Hng.Application.Features.Jobs.Commands;
+
+public class DeleteJobCommand : IRequest<bool>
+{
+    public Guid JobId { get; }
+
+    public DeleteJobCommand(Guid jobId)
+    {
+        JobId = jobId;
+    }
+}
