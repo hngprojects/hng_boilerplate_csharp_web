53219035ddc92851f0ff27e0434d3546b3c3891e
chimaihueze <drkingsleyc@gmail.com>
2024-07-30 21:24:27 +0100

    feat: added query handler

---
+++ src/Hng.Application/Features/Blogs/Handlers/GetBlogByIdQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Hng.Application.Features.Blogs.Dtos;
+using Hng.Application.Features.Blogs.Queries;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+
+namespace Hng.Application.Features.Blogs.Handlers;
+
+public class GetBlogByIdQueryHandler(IRepository<Blog> blogRepository, IMapper mapper) : IRequestHandler<GetBlogByIdQuery, BlogDto>
+{
+    public async Task<BlogDto> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
+    {
+        var blog = await blogRepository.GetBySpec(b => b.Id == request.BlogId);
+        return blog == null ? null : mapper.Map<BlogDto>(blog);
+    }
+}
