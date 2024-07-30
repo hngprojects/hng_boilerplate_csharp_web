5510e0bcd37d524216c8964fae2f584224735913
chimaihueze <drkingsleyc@gmail.com>
2024-07-30 14:14:18 +0100

    feat: added createblogdto

---
+++ src/Hng.Application/Features/Blogs/Dtos/CreateBlogDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using Hng.Domain.Entities;
+using Hng.Domain.Enums;
+
+namespace Hng.Application.Features.Blogs.Dtos;
+
+public class CreateBlogDto
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    [JsonPropertyName("category")]
+    public BlogCategory Category { get; set; }
+}
+    
+    
