a95e2692ead1d7d7901b50c5aec66a74b1cc9974
chimaihueze <drkingsleyc@gmail.com>
2024-07-30 09:48:11 +0100

    feat: added blog entity and dto

---
+++ src/Hng.Application/Features/Blogs/Dtos/BlogDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using Hng.Domain.Entities;
+
+namespace Hng.Application.Features.Blogs.Dtos;
+
+public class BlogDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    [JsonPropertyName("image_url")]
+    public string ImageUrl { get; set; }
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+    [JsonPropertyName("published-date")]
+    public DateTime PublishedDate { get; set; }
+    [JsonPropertyName("is_published")]
+    public bool IsPublished { get; set; }
+    [JsonPropertyName("author")]
+    public User Author { get; set; }
+    [JsonPropertyName("category")]
+    public BlogCategory Category { get; set; }
+}
---
+++ src/Hng.Domain/Entities/Blog.cs
@@ -0,0 +1,12 @@
+﻿namespace Hng.Domain.Entities;
+
+public class Blog : EntityBase
+{
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+    public string Content { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public bool IsPublished { get; set; }
+    public User Author { get; set; }
+    public BlogCategory Category { get; set; }
+}
