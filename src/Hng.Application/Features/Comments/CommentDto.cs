950b37466a88294e04df0f21b7f2f2704a644da8
chimaihueze <drkingsleyc@gmail.com>
2024-07-31 20:47:31 +0100

    added Comment and CommentDto class

---
+++ src/Hng.Application/Features/Comments/CommentDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Comments;
+
+public class CommentDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+}
