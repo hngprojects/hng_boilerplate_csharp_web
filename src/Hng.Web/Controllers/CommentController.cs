3c6c3c881cb2653271053ca72174953643976954
chimaihueze <drkingsleyc@gmail.com>
2024-08-01 10:52:55 +0100

    added Comment Controller, with CreateComment Post Method

---
+++ src/Hng.Web/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using Hng.Application.Features.Comments.Commands;
+using Hng.Application.Features.Comments.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers;
+
+[ApiController]
+[Route("api/v1")]
+public class CommentController : ControllerBase
+{
+    
+    private readonly IMediator _mediator;
+
+    public CommentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [HttpPost("posts/{blogId:guid}/comments")]
+    [ProducesResponseType(typeof(CommentDto), StatusCodes.Status201Created)]
+    public async Task<ActionResult<CommentDto>> CreateComment([FromRoute] Guid blogId, [FromBody] CreateCommentDto body)
+    {
+        var command = new CreateCommentCommand(body, blogId);
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
