757f558d58405c1d142a6938f33141a527a237fd
chimaihueze <drkingsleyc@gmail.com>
2024-08-02 00:00:47 +0100

    added end point for GetCommentsByBlogId

--- src/Hng.Web/Controllers/CommentController.cs
+++ src/Hng.Web/Controllers/CommentController.cs
@@ -1,5 +1,7 @@
 ﻿using Hng.Application.Features.Comments.Commands;
 using Hng.Application.Features.Comments.Dtos;
+using Hng.Application.Features.Comments.Queries;
+using Hng.Application.Shared.Dtos;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,5 +22,19 @@ public class CommentController(IMediator mediator) : ControllerBase
         var command = new CreateCommentCommand(body, blogId);
         var result = await _mediator.Send(command);
         return Ok(result);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(CommentDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(FailureResponseDto<string>), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<CommentDto>>> GetCommentsByBlogId([FromRoute] Guid blogId)
+    {
+        var query = new GetCommentsByBlogIdQuery(blogId);
+        var comments = await _mediator.Send(query);
+        var response = new SuccessResponseDto<IEnumerable<CommentDto>>
+        {
+            Data = comments
+        };
+        return Ok(response);
     }
 }
