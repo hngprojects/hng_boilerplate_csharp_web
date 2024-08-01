255ab82c1fa89c39bd412c9a5a8d0812762863b3
chimaihueze <drkingsleyc@gmail.com>
2024-08-01 10:53:15 +0100

    added CommentMapperProfile

---
+++ src/Hng.Application/Features/Comments/Mappers/CommentMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using Hng.Application.Features.Comments.Dtos;
+using Hng.Domain.Entities;
+using Profile = AutoMapper.Profile;
+
+namespace Hng.Application.Features.Comments.Mappers;
+
+public class CommentMapperProfile : Profile
+{
+    public CommentMapperProfile()
+    {
+        CreateMap<CreateCommentDto, Comment>();
+        CreateMap<Comment, CommentDto>()
+            .ReverseMap();
+    }
+    
+}
