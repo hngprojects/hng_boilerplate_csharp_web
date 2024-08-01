6553cd94380306be8dd907c92dbdf0451e643e04
chimaihueze <drkingsleyc@gmail.com>
2024-08-01 10:41:28 +0100

    created CommentDto

---
+++ src/Hng.Application/Features/Comments/Dtos/CommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Comments.Dtos;
+
+public class CommentDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
