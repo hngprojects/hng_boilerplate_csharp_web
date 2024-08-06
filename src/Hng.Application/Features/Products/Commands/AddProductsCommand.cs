bea657c05c6de961f1bf78bda03188344360e652
Tahir <olawaletech2@gmail.com>
2024-08-06 17:33:27 +0100

    feat: add multiple product feature

---
+++ src/Hng.Application/Features/Products/Commands/AddProductsCommand.cs
@@ -0,0 +1,22 @@
+﻿using Hng.Application.Features.Products.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hng.Application.Features.Products.Commands
+{
+    public class AddProductsCommand : IRequest<ProductsDto>
+    {
+        public AddProductsCommand(string userId, List<ProductCreationDto> productCreation)
+        {
+            UserId = userId;
+            productBody = productCreation;
+        }
+
+        public string UserId { get; }
+        public List<ProductCreationDto> productBody { get; }
+    }
+}
---
+++ src/Hng.Application/Features/Products/Handlers/AddProductsHandler .cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Hng.Application.Features.Products.Commands;
+using Hng.Application.Features.Products.Dtos;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+
+namespace Hng.Application.Features.Products.Handlers
+{
+	public class AddProductsHandler : IRequestHandler<AddProductsCommand, ProductsDto>
+    {
+        private readonly IRepository<Product> _repository;
+        private readonly IMapper _mapper;
+
+        public AddProductsHandler(IRepository<Product> productReposiotry, IMapper mapper)
+        {
+            _repository = productReposiotry;
+            _mapper = mapper;
+        }
+        public async Task<ProductsDto> Handle(AddProductsCommand request, CancellationToken cancellationToken)
+        {
+            var productResponse = new ProductsDto();
+            
+            foreach(var item in request.productBody)
+            {
+				var product = _mapper.Map<Product>(item);
+				product.Id = Guid.NewGuid();
+				product.UserId = Guid.Parse(request.UserId);
+				product.CreatedAt = DateTime.UtcNow;
+				product.UpdatedAt = DateTime.UtcNow;
+				await _repository.AddAsync(product);
+
+                productResponse.Products.Add(_mapper.Map<ProductDto>(product));
+			}
+            await _repository.SaveChanges();
+
+            return productResponse;
+        }
+    }
+}
