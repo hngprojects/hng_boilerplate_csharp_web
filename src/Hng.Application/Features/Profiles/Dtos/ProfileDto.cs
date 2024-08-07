af0d829cc3430a7fcb2a0f6a74e86558f1e6d571
kenelight4u <kenelight4u@gmail.com>
2024-08-07 16:37:25 +0100

    chore: resolve merge conflict

--- src/Hng.Application/Features/Profiles/Dtos/ProfileDto.cs
+++ src/Hng.Application/Features/Profiles/Dtos/ProfileDto.cs
@@ -1,18 +1,30 @@
+using System.Text.Json.Serialization;
+
 namespace Hng.Application.Features.Profiles.Dtos
 {
     public class ProfileDto
     {
+        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+        [JsonPropertyName("phone_number")]
         public string PhoneNumber { get; set; }
+        [JsonPropertyName("avatar_url")]
         public string AvatarUrl { get; set; }
+        [JsonPropertyName("user_id")]
         public Guid UserId { get; set; }
+        [JsonPropertyName("user_name")]
         public string Username { get; set; }
         public string Pronoun { get; set; }
+        [JsonPropertyName("job_title")]
         public string JobTitle { get; set; }
         public string Bio { get; set; }
+        [JsonPropertyName("facebook_link")]
         public string FacebookLink { get; set; }
+        [JsonPropertyName("twitter_link")]
         public string TwitterLink { get; set; }
+        [JsonPropertyName("linkedin_link")]
         public string LinkedinLink { get; set; }
     }
 }
