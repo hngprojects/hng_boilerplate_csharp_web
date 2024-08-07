8c6d0b1d700dc21a5ce3eb2ec73a47307635605b
dorisjenny27 <dorisjenny27@gmail.com>
2024-08-07 19:17:33 +0100

    feat: use JsonPropertyName to map to snake_case

--- src/Hng.Application/Features/ExternalIntegrations/PaymentIntegrations/Paystack/Dtos/Responses/TransactionDto.cs
+++ src/Hng.Application/Features/ExternalIntegrations/PaymentIntegrations/Paystack/Dtos/Responses/TransactionDto.cs
@@ -1,20 +1,44 @@
 ﻿using Hng.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace Hng.Application.Features.ExternalIntegrations.PaymentIntegrations.Paystack.Dtos.Responses
 {
     public class TransactionDto
     {
+        [JsonPropertyName("id")]
         public Guid Id { get; set; }
+
+        [JsonPropertyName("user_id")]
         public Guid UserId { get; set; }
+
+        [JsonPropertyName("product_id")]
         public Guid? ProductId { get; set; }
+
+        [JsonPropertyName("subscription_id")]
         public Guid? SubscriptionId { get; set; }
+
+        [JsonPropertyName("type")]
         public TransactionType Type { get; set; }
+
+        [JsonPropertyName("status")]
         public TransactionStatus Status { get; set; }
+
+        [JsonPropertyName("partners")]
         public TransactionIntegrationPartners Partners { get; set; }
+
+        [JsonPropertyName("amount")]
         public decimal Amount { get; set; }
+
+        [JsonPropertyName("reference")]
         public string Reference { get; set; }
+
+        [JsonPropertyName("created_at")]
         public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("paid_at")]
         public DateTime? PaidAt { get; set; }
+
+        [JsonPropertyName("modified_at")]
         public DateTime? ModifiedAt { get; set; }
     }
 }
