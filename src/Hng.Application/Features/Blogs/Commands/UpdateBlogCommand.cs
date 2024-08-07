d4c5063c329658d0c1ab4023954f62aab5febde2
chimaihueze <drkingsleyc@gmail.com>
2024-08-07 23:30:36 +0100

    added update blog command

---
+++ src/Hng.Application/Features/Blogs/Commands/UpdateBlogCommand.cs
@@ -0,0 +1,9 @@
+﻿using Hng.Application.Features.Blogs.Dtos;
+using MediatR;
+
+namespace Hng.Application.Features.Blogs.Commands;
+
+public class UpdateBlogCommand(BlogDto blog) : IRequest<BlogDto>
+{
+    public BlogDto BlogBody { get; } = blog;
+}
