3076b5071ba86c54e258f90f98442124f34a37ac
dorisjenny27 <dorisjenny27@gmail.com>
2024-08-08 23:14:22 +0100

    feat: Create Timezone Endpoint

---
+++ src/Hng.Application/Features/Timezones/Mappers/TimezoneMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Hng.Application.Features.Timezones.Commands;
+using Hng.Application.Features.Timezones.Dtos;
+using Hng.Domain.Entities;
+
+namespace Hng.Application.Features.Timezones.Mappers
+{
+    public class TimezoneMapperProfile : AutoMapper.Profile
+    {
+        public TimezoneMapperProfile()
+        {
+            CreateMap<Timezone, TimezoneDto>()
+                .ForMember(dest => dest.Timezone, opt => opt.MapFrom(src => src.TimezoneValue))
+                .ReverseMap();
+
+            CreateMap<CreateTimezoneCommand, Timezone>()
+                .ForMember(dest => dest.TimezoneValue, opt => opt.MapFrom(src => src.Timezone));
+        }
+    }
+}
---
+++ src/Hng.Web/Controllers/TimezoneController.cs
@@ -0,0 +1,34 @@
+﻿using Hng.Application.Features.Timezones.Commands;
+using Hng.Application.Features.Timezones.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/timezones")]
+    public class TimezoneController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TimezoneController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Create a new timezone.
+        /// </summary>
+        /// <param name="command">The create timezone command.</param>
+        /// <returns>The created timezone.</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(TimezoneDto), StatusCodes.Status201Created)]
+        public async Task<ActionResult<TimezoneDto>> CreateTimezone([FromBody] CreateTimezoneCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return CreatedAtAction(nameof(CreateTimezone), response);
+        }
+    }
+}
