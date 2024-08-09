ca4b4e39dd1f1290ba0b434c8155908073f22494
AbdulAzeez  Adekoya <abdulazeezadekoya17@gmail.com>
2024-08-09 22:02:20 +0100

    fix:snakecasing

--- src/Hng.Application/Features/HelpCenter/Dtos/HelpCenterResponseDto.cs
+++ src/Hng.Application/Features/HelpCenter/Dtos/HelpCenterResponseDto.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Hng.Application.Features.HelpCenter.Dtos
 {
     public class HelpCenterResponseDto<T>
     {
+        [JsonPropertyName("statuscode")]
         public int StatusCode { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; }
+        [JsonPropertyName("data")]
         public T Data { get; set; }
     }
 }
