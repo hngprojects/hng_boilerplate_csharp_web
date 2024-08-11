87f310cf8c452b9ac6cf4c4deffdb4761be35f02
Aroge Ridwan <104018322+Reliable25@users.noreply.github.com>
2024-08-11 02:34:28 +0100

    feat(product): Get all product for admin

---
+++ src/Hng.Application/Features/Products/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Hng.Application.Features.Products.Dtos;
+using MediatR;
+
+namespace Hng.Application.Features.Products.Queries
+{
+    public class GetAllProductsQuery : IRequest<IEnumerable<ProductResponseDto>>
+    {
+        public Guid OrgId { get; }
+
+        public GetAllProductsQuery(Guid orgId)
+        {
+            OrgId = orgId;
+        }
+    }
+}
