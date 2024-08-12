f1d45402a23848c2bdf7a3b81a379656b54b6006
Aroge Ridwan <104018322+Reliable25@users.noreply.github.com>
2024-08-12 22:08:24 +0100

    chore(notification): snake case correction

--- src/Hng.Application/Features/Notifications/Dtos/GetNotificationsResponseDto.cs
+++ src/Hng.Application/Features/Notifications/Dtos/GetNotificationsResponseDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Hng.Application.Features.Notifications.Dtos
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Notifications.Dtos
 {
     public class GetNotificationsResponseDto
     {
+        [JsonPropertyName("total_notification_count")]
         public int TotalNotificationCount { get; set; }
+        [JsonPropertyName("total_unread_notification_count")]
         public int TotalUnreadNotificationCount { get; set; }
         public List<NotificationDto> Notifications { get; set; }
     }
