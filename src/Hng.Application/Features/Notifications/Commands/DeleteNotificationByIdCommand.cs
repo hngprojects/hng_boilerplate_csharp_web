e9da78abbbdd1a063d335f663ad761857790ec3c
Aroge Ridwan <104018322+Reliable25@users.noreply.github.com>
2024-08-14 00:38:42 +0100

    feat(notification): clear notifications

---
+++ src/Hng.Application/Features/Notifications/Commands/DeleteNotificationByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Hng.Application.Features.Notifications.Commands
+{
+    public class DeleteNotificationByIdCommand : IRequest<bool>
+    {
+        public Guid NotificationId { get; set; }
+
+        public DeleteNotificationByIdCommand(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+    }
+}
