4e5e1dd2e718cf55307a8acb5ffea67201b6c550
chimaihueze <drkingsleyc@gmail.com>
2024-08-14 22:37:52 +0100

    feat: added CreateBlogResponseDto with relevant fields

---
+++ src/Hng.Application/Features/Blogs/Dtos/CreateBlogResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Blogs.Dtos;
+
+public class CreateBlogResponseDto
+{
+    [JsonPropertyName("status_code")]
+    public int StatusCode { get; init; }
+
+    [JsonPropertyName("message")]
+    public string Message { get; init; }
+
+    [JsonPropertyName("data")]
+    public BlogDto Data { get; init; }
+}
