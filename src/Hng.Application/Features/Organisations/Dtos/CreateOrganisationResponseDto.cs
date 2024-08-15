cff213f03e557f1ec5ef2664ad41538d1a75e482
chimaihueze <drkingsleyc@gmail.com>
2024-08-15 09:55:40 +0100

    added CreateOrganisationResponseDto

---
+++ src/Hng.Application/Features/Organisations/Dtos/CreateOrganisationResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Organisations.Dtos;
+
+public class CreateOrganisationResponseDto
+{
+    [JsonPropertyName("status_code")]
+    public int StatusCode { get; set; }
+
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    [JsonPropertyName("data")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public OrganizationDto Data { get; set; }
+}
