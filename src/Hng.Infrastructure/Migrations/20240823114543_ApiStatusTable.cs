e16a7f83039442120d7f4873f3bd9b30a825c31a
kenelight4u <kenelight4u@gmail.com>
2024-08-23 12:50:27 +0100

    feat: configured ApiStatus database

---
+++ src/Hng.Infrastructure/Migrations/20240823114543_ApiStatusTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hng.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiStatusTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ApiGroup = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    ResponseTime = table.Column<long>(type: "bigint", nullable: false),
+                    Details = table.Column<string>(type: "text", nullable: true),
+                    LastChecked = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiStatuses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiStatuses");
+        }
+    }
+}
