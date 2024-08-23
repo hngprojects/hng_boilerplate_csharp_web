27fc9b56d866de3e4ed4c4ccafa03e59302d6120
AbdulAzeez  Adekoya <abdulazeezadekoya17@gmail.com>
2024-08-24 00:28:31 +0100

    Implement GetStatus endpoint and unit tests using CQRS
    - Added GetStatus endpoint to retrieve API statuses with pagination.
    - Implemented GetStatusQuery and GetStatusQueryHandler for handling the request.
    - Added default pagination handling in GetStatusQueryHandler.
    - Created unit tests for GetStatus endpoint using xUnit and Moq.
    - Ensured tests cover successful retrieval, empty result scenarios, and error handling.

---
+++ src/Hng.Application/Features/ApiStatuses/Handlers/Queries/GetAllApiStatusesHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Hng.Application.Features.ApiStatuses.Dtos.Requests;
+using Hng.Application.Features.ApiStatuses.Dtos.Responses;
+using Hng.Application.Features.Timezones.Dtos;
+using Hng.Application.Features.Timezones.Queries;
+using Hng.Application.Shared.Dtos;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hng.Application.Features.ApiStatuses.Handlers.Queries
+{
+    public class GetAllApiStatusesHandler : IRequestHandler<GetAllApiStatusesQueries, PaginatedResponseDto<List<ApiStatusResponseDto>>>
+    {
+        private readonly IRepository<ApiStatus> _apistatusRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllApiStatusesHandler(IRepository<ApiStatus> apistatusRepository, IMapper mapper)
+        {
+            _apistatusRepository = apistatusRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedResponseDto<List<ApiStatusResponseDto>>> Handle(GetAllApiStatusesQueries request, CancellationToken cancellationToken)
+        {
+            var pageNumber = request.PageNumber > 0 ? request.PageNumber : 1;
+            var pageSize = request.PageSize > 0 ? request.PageSize : 10;
+            var apiStatus = await _apistatusRepository.GetAllAsync();
+            var apiStatusDtos = _mapper.Map<List<ApiStatusResponseDto>>(apiStatus);
+            var paginatedApiStatus = PagedListDto<ApiStatusResponseDto>.ToPagedList(
+                apiStatusDtos,
+                pageNumber,
+                pageSize
+            );
+
+            return new PaginatedResponseDto<List<ApiStatusResponseDto>>
+            {
+                Data = paginatedApiStatus.ToList(),
+                Metadata = paginatedApiStatus.MetaData
+            };
+        }
+    }
+}
