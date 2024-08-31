eba3405ec63101fd01cae12e9976eafeafafe3ec
chimaihueze <drkingsleyc@gmail.com>
2024-08-31 10:01:41 +0100

    added Squeeze class and respective dtos

---
+++ src/Hng.Application/Features/Squeeze/Dtos/SqueezeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Squeeze.Dtos;
+
+public class SqueezeDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+        
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+}
---
+++ src/Hng.Domain/Entities/Squeeze.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hng.Domain.Entities;
+
+public class Squeeze
+{
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+}
