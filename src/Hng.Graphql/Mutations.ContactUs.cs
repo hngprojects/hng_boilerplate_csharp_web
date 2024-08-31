ae58b59ca56bd96e5fbad6c784103d701d317abf
dorisjenny27 <dorisjenny27@gmail.com>
2024-08-31 12:35:15 +0100

    feat: implement contact us endpoints in graphql

---
+++ src/Hng.Graphql/Mutations.ContactUs.cs
@@ -0,0 +1,25 @@
+﻿using Hng.Application.Features.ContactsUs.Command;
+using Hng.Application.Features.ContactsUs.Dtos;
+using HotChocolate.Authorization;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng.Graphql
+{
+    public partial class Mutations
+    {
+        [Authorize]
+        public async Task<ContactResponse<ContactUsResponseDto>> CreateContactMessage(ContactUsRequestDto contactUsRequest, [FromServices] IMediator mediator)
+        {
+            var command = new CreateContactUsCommand(contactUsRequest);
+            return await mediator.Send(command);
+        }
+
+        [Authorize]
+        public async Task<ContactResponse<object>> DeleteContactMessage(Guid id, [FromServices] IMediator mediator)
+        {
+            var command = new DeleteContactUsCommand(id);
+            return await mediator.Send(command);
+        }
+    }
+}
