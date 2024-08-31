3780c7aa37171f980a1e2c2062ea523e7cf87cc9
dorisjenny27 <dorisjenny27@gmail.com>
2024-08-31 13:42:42 +0100

    subscriptions

--- src/Hng.Graphql/Queries.Subscriptions.cs
+++ src/Hng.Graphql/Queries.Subscriptions.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hng.Application.Features.Subscriptions.Dtos.Requests;
+using Hng.Application.Features.Subscriptions.Dtos.Responses;
+using Hng.Application.Features.Subscriptions.Queries;
+using Hng.Application.Shared.Dtos;
+using HotChocolate.Authorization;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Hng.Graphql
 {
-    public class Queries
+    public partial class Queries
     {
+        [Authorize]
+        public async Task<SubscriptionDto> GetSubscriptionByOrganizationId(Guid organizationId, [FromServices] IMediator mediator)
+        {
+            var response = new GetSubscriptionByOrganizationIdQuery(organizationId);
+            return await mediator.Send(response);
+        }
+
+        [Authorize]
+        public async Task<SubscriptionDto> GetSubscriptionByUserId(Guid userId, [FromServices] IMediator mediator)
+        {
+            var response = new GetSubscriptionByUserIdQuery(userId);
+            return await mediator.Send(response);
+        }
+
+        [Authorize]
+        public async Task<PagedListDto<SubscriptionDto>> GetSubscriptions(GetSubscriptionsQueryParameters parameters, [FromServices] IMediator mediator)
+        {
+            var subscriptions = new GetSubscriptionsQuery(parameters);
+            return await mediator.Send(subscriptions);
+        }
     }
 }
