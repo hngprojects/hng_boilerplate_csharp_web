83f2098a18ae142f1c02151690bcd270609f7641
Godand <aezeigwe2003@gmail.com>
2024-08-31 19:07:46 +0200

    feat(graphql): implement newsletter subscription resolver

--- src/Hng.Graphql/Features/Queries/Queries.Admin.cs
+++ src/Hng.Graphql/Features/Queries/Queries.Admin.cs
@@ -1,6 +1,7 @@
 ﻿using Hng.Application.Features.SuperAdmin.Dto;
 using Hng.Application.Features.SuperAdmin.Queries;
 using Hng.Application.Shared.Dtos;
+using HotChocolate.Authorization;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace Hng.Graphql
 {
     public partial class Queries
     {
+        [Authorize]
         public async Task<PagedListDto<UserDto>> GetUsersBySearch(UsersQueryParameters parameters, [FromServices] IMediator mediator)
         {
             var users = new GetUsersBySearchQuery(parameters);
