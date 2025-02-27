b4666fd79ea4426cc6f00f81153083dae23eebce
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-28 02:19:22 +0300

    feat: IsDeleted column to NewsLetterSubscriber for soft deletion

---
+++ src/Hng.Infrastructure/Migrations/20250227230159_AddIsDeletedToNewsLetterSubscriber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hng.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedToNewsLetterSubscriber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "NewsLetterSubscribers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "NewsLetterSubscribers");
+        }
+    }
+}
