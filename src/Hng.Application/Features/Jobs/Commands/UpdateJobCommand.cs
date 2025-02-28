1955ba5cc192559711251608141a17e5504e3f16
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-02-28 23:10:03 +0100

    Feat: Add update job command

---
+++ src/Hng.Application/Features/Jobs/Commands/UpdateJobCommand.cs
@@ -0,0 +1,21 @@
+﻿using Hng.Application.Features.Jobs.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hng.Application.Features.Jobs.Commands
+{
+    public class UpdateJobCommand : IRequest<UpdateJobDto>
+    {
+        public UpdateJobDto UpdateJob { get; set; }
+        public Guid JobId { get; set; }
+        public UpdateJobCommand(UpdateJobDto updateJob, Guid jobId)
+        {
+            UpdateJob = updateJob;
+            JobId = jobId;
+        }
+    }
+}
