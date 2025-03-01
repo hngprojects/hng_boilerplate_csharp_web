a0929593dab4c51a64c8483fb02dc4e626d37dc1
Odili1 <ezedigwegerald1@gmail.com>
2025-03-01 15:49:01 +0100

    fix: Add filter by category

--- src/Hng.Application/Features/Products/Handlers/GetAllProductsHandler.cs
+++ src/Hng.Application/Features/Products/Handlers/GetAllProductsHandler.cs
@@ -10,17 +10,26 @@ namespace Hng.Application.Features.Products.Handlers
     public class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<ProductResponseDto>>
     {
         private readonly IRepository<Product> _productRepository;
+        private readonly IRepository<Category> _categoryRepository;
         private readonly IMapper _mapper;
 
-        public GetAllProductsHandler(IRepository<Product> productRepository, IMapper mapper)
+        public GetAllProductsHandler(IRepository<Product> productRepository, IRepository<Category> categoryRepository, IMapper mapper)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<ProductResponseDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
         {
-            var products = await _productRepository.GetAllBySpec(u => u.OrganizationId == request.OrgId);
+            var validCategories = await _categoryRepository.GetAllAsync();
+            var categoriesNames = validCategories.Select(category => category.Name).ToHashSet();
+
+            if (!string.IsNullOrEmpty(request.Category) && !categoriesNames.Contains(request.Category)){
+                throw new ArgumentException($"Invalid category '{request.Category}' provided.");
+            }
+
+            var products = await _productRepository.GetAllBySpec(product => product.OrganizationId == request.OrgId && (string.IsNullOrEmpty(request.Category) || product.Category == request.Category));
             var productDtos = _mapper.Map<IEnumerable<ProductResponseDto>>(products);
 
             foreach (var product in productDtos)
