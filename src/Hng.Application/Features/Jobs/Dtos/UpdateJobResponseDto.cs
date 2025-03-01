c045ea82692d702f3d135bc08c4a9676412726b0
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-03-01 01:47:40 +0100

    feat: add Update job response dto and mapper in IMapper

--- src/Hng.Application/Features/Jobs/Dtos/UpdateJobResponseDto.cs
+++ src/Hng.Application/Features/Jobs/Dtos/UpdateJobResponseDto.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Hng.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Hng.Application.Features.Jobs.Dtos
 {
-    internal class UpdateJobResponseDto
+    public class UpdateJobResponseDto
     {
+        [JsonPropertyName("status_code")]
+        public int StatusCode { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("data")]
+        public Job Data { get; set; }
     }
 }
