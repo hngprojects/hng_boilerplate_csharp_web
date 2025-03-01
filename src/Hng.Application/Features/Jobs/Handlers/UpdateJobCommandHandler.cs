f9d93646bdf7b3af64cc62b25ccc7523f44df34f
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-03-01 01:35:19 +0100

    Feat: Add update job command handler

---
+++ src/Hng.Application/Features/Jobs/Handlers/UpdateJobCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Hng.Application.Features.Jobs.Commands;
+using Hng.Application.Features.Jobs.Dtos;
+using Hng.Domain.Entities;
+using Hng.Infrastructure.Repository.Interface;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Mysqlx.Notice.Warning.Types;
+
+namespace Hng.Application.Features.Jobs.Handlers
+{
+    public class UpdateJobCommandHandler
+    {
+
+    }
+}
