a4195e6a0f67d7d55114e82076b47990e1274e27
OBE96 <brightelo1@gmail.com>
2025-03-02 22:37:42 +0100

    feat/Implement an endpoint to export all users in an CSV file(only for super admin role)

--- src/Hng.Web/Controllers/UserController.cs
+++ src/Hng.Web/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 using Hng.Application.Features.UserManagement.Commands;
 using Hng.Application.Features.UserManagement.Dtos;
 using Hng.Application.Features.UserManagement.Queries;
+using Hng.Application.Shared.Dtos;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Hng.Web.Controllers;
 
@@ -36,6 +38,29 @@ public class UserController(IMediator mediator) : ControllerBase
     {
         var users = await _mediator.Send(new GetUsersQuery());
         return Ok(users);
+    }
+
+    [HttpGet("export/csv")]
+    //[Authorize(Roles = "SuperAdmin")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(FailureResponseDto<object>), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(FailureResponseDto<object>), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> ExportUsersToCsv()
+    {
+        try
+        {
+            var csvBytes = await _mediator.Send(new ExportUsersToCsvQuery());
+            var fileName = $"users_export_{DateTime.UtcNow:yyyy-MM-dd}.csv";
+            return File(csvBytes, MediaTypeNames.Application.Octet, fileName);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Unauthorized(new FailureResponseDto<object>
+            {
+                StatusCode = 401,
+                Message = "Unauthorized access. You must have Super Admin privileges to export user data.",
+            });
+        }
     }
 
     [HttpPut("organisations/{organisationId:guid}")]
